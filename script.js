1e907ae5327a6e0241f68f4232853fe2ff5392b5
Hoffmann-Ferreira <andreihf@outlook.com>
2022-04-15 23:23:47 -0300

    Funções js feitas

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-window.onload = function() {
-    alert("Olá mundo do JavaScript para Web");
-};
-const btnTexto = document.querySelector("#btn-texto"); // Captura o botão no DOM pelo id
+// const botao = document.querySelector("#botao");
+// let contador = 0;
 
-btnTexto.addEventListener("click", function () { // Adiciona um evento de click no botão capturado e assim que for clicado, executa a função.
-  const tagH1 = document.createElement("h1"); // Cria uma tag h1 e adiciona a let tagH1
-  const h1Text = document.createTextNode("Texto criado no JS"); // Cria um texto e adiciona a let h1Text
+// function modificaFotosTexto(){
+//   let imagens = ['pichu.png', 'pikachu.jpg', 'raichu.jpg'];
 
-  tagH1.appendChild(h1Text); // Adiciona a tag H1 o texto criado acima
+//   let texto = document.querySelector(".teste");
+//   texto.innerHTML = `<h1> Testando ${contador}</h1>    
+//   <img src="/Projeto-1-HTML/assets/${imagens[contador]}">`;
+//   contador++;
+//   if(contador == imagens.length){
+//     contador = 0;
+//   }
+  
+// };
 
-  document.body.appendChild(tagH1); // Adiciona ao body a h1 criada, com seu texto.
-});
+// botao.addEventListener("click", modificaFotosTexto);
