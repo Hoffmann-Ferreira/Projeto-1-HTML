75197fde1240a021ce2d403f6445639431ef4172
Hoffmann-Ferreira <andreihf@outlook.com>
2022-04-19 11:30:20 -0300

    Foramtação com prettier

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const botao = document.querySelector("#botao");
 let contador = 0;
 
-function modificaFotosTexto(){
-  const imagens = ['anakin1.png', 'anakin21.png', 'anakin3.png', 'vader.png', 'old.png'];
-  const textos = ['ESCRAVO', 'JOVEM JEDI', 'LORD VADER', 'DARTH VADER', 'ESPÍRITO DA FORÇA'];
+function modificaFotosTexto() {
+  const imagens = [
+    "anakin1.png",
+    "anakin21.png",
+    "anakin3.png",
+    "vader.png",
+    "old.png",
+  ];
+  const textos = [
+    "ESCRAVO",
+    "JOVEM JEDI",
+    "LORD VADER",
+    "DARTH VADER",
+    "ESPÍRITO DA FORÇA",
+  ];
 
   let fotosTexto = document.querySelector(".personagem");
 
@@ -11,11 +23,10 @@ function modificaFotosTexto(){
   <img src="/Projeto-1-HTML/assets/${imagens[contador]}">`;
 
   contador++;
-  
-  if(contador == imagens.length){
+
+  if (contador == imagens.length) {
     contador = 0;
-  };
-  
-};
+  }
+}
 
 botao.addEventListener("click", modificaFotosTexto);
